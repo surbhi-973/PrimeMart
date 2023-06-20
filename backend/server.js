371fe96c9d2b25998b0270e1817c77dee09ef7c3
Surbhi Pandey <114894985+surbhi-973@users.noreply.github.com>
2023-06-20 22:32:34 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -29,18 +29,20 @@ app.use(errorHandler)
 const PORT = process.env.PORT || 5000
 
 if (process.env.NODE_ENV === 'production') {
-    // set static folder
-    app.use(express.static(path.join(__dirname, '/uploads')));
-
-    //any route that is not api will be redirected to index.html
+    const __dirname = path.resolve();
+    app.use('/uploads', express.static('/var/data/uploads'));
+    app.use(express.static(path.join(__dirname, '/frontend/build')));
+  
     app.get('*', (req, res) =>
-        res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+      res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
     );
-} else {
+  } else {
+    const __dirname = path.resolve();
+  app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
     app.get('/', (req, res) => {
-        res.send('API is running...')
+      res.send('API is running....');
     });
-}
+  }
 
 app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} on port ${PORT}`.yellow.bold))
 
