db215def4d447a1cb73f3f01934a197413bbc573
Surbhi Pandey <114894985+surbhi-973@users.noreply.github.com>
2023-06-24 17:51:28 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 import express from 'express'
 import path from 'path'
+import morgan from 'morgan'
 import dotenv from 'dotenv'
 import colors from 'colors'
 import { notFound, errorHandler } from './middleware/errorMiddleware.js'
@@ -15,11 +16,11 @@ connectDB()
 
 const app = express()
 
-app.use(express.json())
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'))
+}
 
-app.get('/', (req, res) => {
-      res.send('API is running...')
-  });
+app.use(express.json())
 
 app.use('/api/products', productRoutes)
 app.use('/api/users', userRoutes)
@@ -33,21 +34,20 @@ app.use(errorHandler)
 
 const PORT = process.env.PORT || 5000
 
+const __dirname = path.resolve()
+app.use('/uploads', express.static(path.join(__dirname, '/uploads')))
+
 if (process.env.NODE_ENV === 'production') {
-    const __dirname = path.resolve();
-    app.use('/uploads', express.static('/var/data/uploads'));
-    app.use(express.static(path.join(__dirname, '/frontend/build')));
-  
-    app.get('*', (req, res) =>
-      res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
-    );
-  } else {
-    const __dirname = path.resolve();
-  app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
-    app.get('/', (req, res) => {
-      res.send('API is running....');
-    });
-  }
+  app.use(express.static(path.join(__dirname, '/frontend/build')))
+
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+  )
+} else {
+  app.get('/', (req, res) => {
+    res.send('API is running....')
+  })
+}
 
 app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} on port ${PORT}`.yellow.bold))
 
